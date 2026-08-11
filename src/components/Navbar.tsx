db2224c77d20a730675fd4d43725af5e3eb211d3
mxilia <ravintkosol@gmail.com>
2026-08-11 20:55:17 +0700

    feat: update global components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import { AuthSection } from '@/features/auth/components/auth-section'
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full h-16 items-center flex">
+    <div className="fixed w-full h-20 items-center flex justify-between pr-20 border-b border-blue-300/20 bg-black/40 backdrop-blur-xl backdrop-saturate-150">
+      <div></div>
       <AuthSection />
     </div>
   )
--- src/components/overlay/overlay.tsx
+++ src/components/overlay/overlay.tsx
@@ -1,24 +1,37 @@
 'use client'
 
+import { AnimatePresence, motion } from 'motion/react'
 import { useOverlayStore } from './overlay.store'
 
 export const Overlay = () => {
   const overlays = useOverlayStore((state) => state.overlays)
   const close = useOverlayStore((state) => state.close)
-  if (overlays.length === 0) return null
   return (
-    <>
-      <div className="fixed inset-0 z-40 flex items-center justify-center">
-        {overlays.map((overlay, index) => (
-          <div key={overlay.id}>{index !== overlays.length - 1 && <>{overlay.node}</>}</div>
-        ))}
-      </div>
-      <div
-        className="fixed inset-0 z-50 flex items-center justify-center bg-black/40"
-        onClick={() => close()}
-      >
-        <div onClick={(e) => e.stopPropagation()}>{overlays.at(-1)?.node}</div>
-      </div>
-    </>
+    <AnimatePresence>
+      {overlays.length > 0 && (
+        <>
+          <motion.div
+            className="fixed inset-0 z-40 flex items-center justify-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {overlays.map((overlay, index) => (
+              <div key={overlay.id}>{index !== overlays.length - 1 && <>{overlay.node}</>}</div>
+            ))}
+          </motion.div>
+
+          <motion.div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black/40"
+            onClick={() => close()}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <div onClick={(e) => e.stopPropagation()}>{overlays.at(-1)?.node}</div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
